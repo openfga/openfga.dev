77c3adda566342b8d588395feec7cc52da640466
Adrian Tam <adrian.tam@auth0.com>
2022-06-10 13:27:21 -0400

    fix: link on hero landing page to github
    Change link to https://github.com/openfga
    
    Close https://github.com/openfga/openfga.dev/issues/36

--- src/features/LandingPage/HeroHeader/index.tsx
+++ src/features/LandingPage/HeroHeader/index.tsx
@@ -66,7 +66,7 @@ const HeroHeader = () => {
                 </svg>
                 Quick start
               </ButtonLink>
-              <ButtonLink href="https://github.com/openfga/openfga">
+              <ButtonLink href="https://github.com/openfga">
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path
                     fillRule="evenodd"
