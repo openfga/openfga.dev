57e60a0f9a5f17034f5742ac6b27c90850d44dce
Mat Dupont <dupont.mat.l@gmail.com>
2022-06-13 09:53:17 -0400

    chore: added comments

--- src/features/LandingPage/StyleGrid/index.tsx
+++ src/features/LandingPage/StyleGrid/index.tsx
@@ -4,6 +4,7 @@ import styles from './StyleGrid.module.css';
 
 const StyleGrid = () => (
   <div className={styles.grid}>
+    {/* The grid defines the layout with some cascading styles for the borders. These divs are, indeed, meant to be empty */}
     <div />
     <div />
   </div>
