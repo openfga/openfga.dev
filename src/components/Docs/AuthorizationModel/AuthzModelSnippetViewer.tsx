e50cdb1a35843b1c04c58895f1a359b93fefd3e0
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2022-06-13 13:29:14 -0400

    feat: add write authorization model viewer component

--- src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
+++ src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
@@ -1,25 +1,30 @@
+import React from 'react';
+
+import { TypeDefinitions } from '@auth0/fga';
+import Link from "@docusaurus/Link";
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import Admonition from '@theme/Admonition';
 
-import React from 'react';
-import { TypeDefinitions } from '@auth0/fga';
-import { SyntaxFormat } from './SyntaxTransformer';
+import { SyntaxFormat, WriteAuthzModelViewer } from "@components/Docs";
 import { AuthzModelCodeBlock } from './AuthzModelCodeBlock';
 
 type AuthzModelSnippetViewerProps = {
-  // Namespace configuration in api syntax
+  // Authorization Model in api syntax
   configuration: TypeDefinitions;
   // optional description
   description?: string;
   onlyShow?: SyntaxFormat;
+  showWrite?: boolean;
 };
 
-const AuthzModelSnippetViewer: React.FC<AuthzModelSnippetViewerProps> = ({ configuration, onlyShow }) => {
+const AuthzModelSnippetViewer: React.FC<AuthzModelSnippetViewerProps> = ({ configuration, onlyShow, showWrite }) => {
   if (onlyShow) {
     return <AuthzModelCodeBlock configuration={configuration} syntaxFormat={onlyShow} />;
   }
 
   return (
+    <>
     <Tabs groupId="dsl">
       <TabItem value="dsl" label="DSL">
         <AuthzModelCodeBlock configuration={configuration} syntaxFormat={SyntaxFormat.Friendly2} />
@@ -28,6 +33,20 @@ const AuthzModelSnippetViewer: React.FC<AuthzModelSnippetViewerProps> = ({ confi
         <AuthzModelCodeBlock configuration={configuration} syntaxFormat={SyntaxFormat.Api} />
       </TabItem>
     </Tabs>
+    {
+      showWrite ?
+        <details>
+          <summary>Write the Authorization Model</summary>
+
+          <Admonition type="note">
+            <div>The OpenFGA API only accepts an authorization model in the API&#39;s JSON syntax.</div>
+            <div>To convert between the API Syntax and the friendly DSL, you can use the <Link href={"https://github.com/openfga/syntax-transformer/"}>syntax transformer</Link> or <Link href={"https://play.fga.dev"}>Auth0 FGA&#39;s Playground</Link>.</div>
+          </Admonition>
+
+          <WriteAuthzModelViewer authorizationModel={configuration} skipSetup={true} />
+        </details> : undefined
+    }
+    </>
   );
 };
 
