b7bc8bf78e71903254c7dc284c9a29a9a8657abb
Adrian Tam <adrian.tam@auth0.com>
2022-06-14 11:39:34 -0400

    fix: run prettier and factor out functions
    This simplify logic for writeAuthZModelViewer

--- src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
+++ src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import { TypeDefinitions } from '@auth0/fga';
-import Link from "@docusaurus/Link";
+import Link from '@docusaurus/Link';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import Admonition from '@theme/Admonition';
 
-import { SyntaxFormat, WriteAuthzModelViewer } from "@components/Docs";
+import { SyntaxFormat, WriteAuthzModelViewer } from '@components/Docs';
 import { AuthzModelCodeBlock } from './AuthzModelCodeBlock';
 
 type AuthzModelSnippetViewerProps = {
@@ -25,27 +25,30 @@ const AuthzModelSnippetViewer: React.FC<AuthzModelSnippetViewerProps> = ({ confi
 
   return (
     <>
-    <Tabs groupId="dsl">
-      <TabItem value="dsl" label="DSL">
-        <AuthzModelCodeBlock configuration={configuration} syntaxFormat={SyntaxFormat.Friendly2} />
-      </TabItem>
-      <TabItem value="json" label="JSON">
-        <AuthzModelCodeBlock configuration={configuration} syntaxFormat={SyntaxFormat.Api} />
-      </TabItem>
-    </Tabs>
-    {
-      showWrite ?
+      <Tabs groupId="dsl">
+        <TabItem value="dsl" label="DSL">
+          <AuthzModelCodeBlock configuration={configuration} syntaxFormat={SyntaxFormat.Friendly2} />
+        </TabItem>
+        <TabItem value="json" label="JSON">
+          <AuthzModelCodeBlock configuration={configuration} syntaxFormat={SyntaxFormat.Api} />
+        </TabItem>
+      </Tabs>
+      {showWrite ? (
         <details>
           <summary>Write the Authorization Model</summary>
 
           <Admonition type="note">
             <div>The OpenFGA API only accepts an authorization model in the API&#39;s JSON syntax.</div>
-            <div>To convert between the API Syntax and the friendly DSL, you can use the <Link href={"https://github.com/openfga/syntax-transformer/"}>syntax transformer</Link> or <Link href={"https://play.fga.dev"}>Auth0 FGA&#39;s Playground</Link>.</div>
+            <div>
+              To convert between the API Syntax and the friendly DSL, you can use the{' '}
+              <Link href={'https://github.com/openfga/syntax-transformer/'}>syntax transformer</Link> or{' '}
+              <Link href={'https://play.fga.dev'}>Auth0 FGA&#39;s Playground</Link>.
+            </div>
           </Admonition>
 
           <WriteAuthzModelViewer authorizationModel={configuration} skipSetup={true} />
-        </details> : undefined
-    }
+        </details>
+      ) : undefined}
     </>
   );
 };
--- src/components/Docs/SnippetViewer/WriteAuthzModelViewer.tsx
+++ src/components/Docs/SnippetViewer/WriteAuthzModelViewer.tsx
@@ -1,8 +1,7 @@
 import { getFilteredAllowedLangs, SupportedLanguage, LanguageMappings } from './SupportedLanguage';
 import { defaultOperationsViewer } from './DefaultTabbedViewer';
 import assertNever from 'assert-never/index';
-import { AuthorizationModel } from "@auth0/fga";
-
+import { AuthorizationModel } from '@auth0/fga';
 
 interface WriteAuthzModelViewerOpts {
   authorizationModel: AuthorizationModel;
@@ -10,44 +9,64 @@ interface WriteAuthzModelViewerOpts {
   allowedLanguages?: SupportedLanguage[];
 }
 
-function writeAuthZModelViewer(lang: SupportedLanguage, opts: WriteAuthzModelViewerOpts, languageMappings: LanguageMappings) {
-  switch (lang) {
-    case SupportedLanguage.CURL: {
-      return `curl -X POST $FGA_API_URL/stores/$FGA_STORE_ID/write \\
+function writeAuthZModelViewerCurl(authorizationModel: AuthorizationModel): string {
+  return `curl -X POST $FGA_API_URL/stores/$FGA_STORE_ID/write \\
   -H "Authorization: Bearer $FGA_BEARER_TOKEN" \\ # Not needed if service does not require authorization
   -H "content-type: application/json" \\
-  -d '${JSON.stringify(opts.authorizationModel)}'`;
+  -d '${JSON.stringify(authorizationModel)}'`;
+}
+
+function writeAuthZModelViewerJS(authorizationModel: AuthorizationModel): string {
+  return `
+await fgaClient.writeAuthorizationModel(${JSON.stringify(authorizationModel, null, 2)});`;
+}
+
+function writeAuthZModelViewerGo(authorizationModel: AuthorizationModel, apiName: string): string {
+  /* eslint-disable no-tabs */
+  return `
+  var typeDefinitionsString = ${JSON.stringify(JSON.stringify(authorizationModel))}
+  var typeDefinitions TypeDefinitions
+  if err := json.Unmarshal([]byte(typeDefinitionsString), &typeDefinitions); err != nil {
+      t.Errorf("%v", err)
+      // .. Handle error
+      return
+  }
+
+  _, response, err := fgaClient.${apiName}.WriteAuthorizationModel(context.Background()).TypeDefinitions(typeDefinitions).Execute()
+  if err != nil {
+      // .. Handle error
+  }
+  `;
+}
+
+function writeAuthZModelViewerDotnet(authorizationModel: AuthorizationModel): string {
+  return `
+  var modelJson = ${JSON.stringify(JSON.stringify(authorizationModel))};
+  var body = JsonSerializer.Deserialize<ReadAuthorizationModelsResponse>(modelJson);
+
+  await fgaClient.WriteAuthorizationModel(body);`;
+}
+
+function writeAuthZModelViewer(
+  lang: SupportedLanguage,
+  opts: WriteAuthzModelViewerOpts,
+  languageMappings: LanguageMappings,
+) {
+  switch (lang) {
+    case SupportedLanguage.CURL: {
+      return writeAuthZModelViewerCurl(opts.authorizationModel);
     }
 
     case SupportedLanguage.JS_SDK: {
-      return `
-await fgaClient.writeAuthorizationModel(${JSON.stringify(opts.authorizationModel, null, 2)});`;
+      return writeAuthZModelViewerJS(opts.authorizationModel);
     }
 
     case SupportedLanguage.GO_SDK: {
-      /* eslint-disable no-tabs */
-      return `
-var typeDefinitionsString = ${JSON.stringify(JSON.stringify(opts.authorizationModel))}
-var typeDefinitions TypeDefinitions
-if err := json.Unmarshal([]byte(typeDefinitionsString), &typeDefinitions); err != nil {
-    t.Errorf("%v", err)
-    // .. Handle error
-    return
-}
-
-_, response, err := fgaClient.${languageMappings['go'].apiName}.WriteAuthorizationModel(context.Background()).TypeDefinitions(typeDefinitions).Execute()
-if err != nil {
-    // .. Handle error
-}
-`;
+      return writeAuthZModelViewerGo(opts.authorizationModel, languageMappings['go'].apiName);
     }
 
     case SupportedLanguage.DOTNET_SDK: {
-      return `
-var modelJson = ${JSON.stringify(JSON.stringify(opts.authorizationModel))};
-var body = JsonSerializer.Deserialize<ReadAuthorizationModelsResponse>(modelJson);
-
-await fgaClient.WriteAuthorizationModel(body);`;
+      return writeAuthZModelViewerDotnet(opts.authorizationModel);
     }
 
     case SupportedLanguage.RPC:
