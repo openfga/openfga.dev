0c57cd05bd0cb88a2f3467f70dbb6fbf00832cfe
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2022-06-15 14:55:18 -0400

    chore(deps): switch fga package to @openfga/sdk

--- src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
+++ src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { TypeDefinitions } from '@auth0/fga';
+import { TypeDefinitions } from '@openfga/sdk';
 import Link from '@docusaurus/Link';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
