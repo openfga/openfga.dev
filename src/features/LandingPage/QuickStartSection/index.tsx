3c609504aefde484719df7ed87972d92fe388fd9
Matthew Pereira <mail@matthewpereira.com>
2022-06-20 11:37:05 -0300

    fix: update quickstart area for mobile and add code

--- src/features/LandingPage/QuickStartSection/index.tsx
+++ src/features/LandingPage/QuickStartSection/index.tsx
@@ -15,7 +15,11 @@ const QuickStartSection = () => {
         <div className={styles.content}>
           <h2 className="section_heading">Quick Start</h2>
           <p className={styles.highlight}>Trying OpenFGA is as easy as...</p>
-          <p>Run the curl command on the right in a terminal that is running on a machine with Docker installed.</p>
+          <p>Run the following snippet in a terminal in an environment with Docker installed:</p>
+          <pre className={styles.dockerSnippet}>
+            docker pull openfga/openfga && \<br />
+            docker run -p 8080:8080 openfga/openfga run
+          </pre>
           <p>
             OpenFGA will be running at localhost:8080 on your machine. Learn about other options and next steps in the
             project <Link href="https://github.com/openfga/openfga">README.md</Link> or{' '}
@@ -26,7 +30,7 @@ const QuickStartSection = () => {
             <Link to="./docs/modeling">documentation</Link>.
           </p>
         </div>
-        <div className={styles.graphic}>
+        <div className={styles.terminalContainer}>
           <video width="646" preload="auto" autoPlay muted loop className={styles.terminalImg} poster={terminalImg}>
             <source src={terminalWebm} type="video/webm" />
             <source src={terminalMp4} type="video/mp4" />
