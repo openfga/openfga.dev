3cf49296f6de9afc05c6729a75cdc23e7f25e8ca
Matthew Pereira <mail@matthewpereira.com>
2022-06-21 16:53:28 -0300

    feat: video for quick start (#107)
    * feat: video for quick start
    
    * fix: update video content
    
    * fix: update quickstart area for mobile and add code
    
    * fix: change px to rem
    
    * fix: padding, csr fix
    
    * fix: update terminal image
    
    Co-authored-by: Adrian Tam <adrian.tam@auth0.com>
    Co-authored-by: Mat Dupont <dupont.mat.l@gmail.com>

--- src/features/LandingPage/QuickStartSection/index.tsx
+++ src/features/LandingPage/QuickStartSection/index.tsx
@@ -1,7 +1,10 @@
 import Link from '@docusaurus/Link';
 import * as React from 'react';
 import { Section } from '@components/Section';
-import QuickStartImg from '@static/img/openfga_quickstart_terminal.svg';
+
+import terminalMp4 from './terminal.mp4';
+import terminalWebm from './terminal.webm';
+import terminalImg from './terminal.png';
 
 import styles from './QuickStartSection.module.css';
 
@@ -12,7 +15,11 @@ const QuickStartSection = () => {
         <div className={styles.content}>
           <h2 className="section_heading">Quick Start</h2>
           <p className={styles.highlight}>Trying OpenFGA is as easy as...</p>
-          <p>Run the curl command on the right in a terminal that is running on a machine with Docker installed.</p>
+          <p>Run the following snippet in a terminal in an environment with Docker installed:</p>
+          <pre className={styles.dockerSnippet}>
+            docker pull openfga/openfga && \<br />
+            docker run -p 8080:8080 openfga/openfga run
+          </pre>
           <p>
             OpenFGA will be running at localhost:8080 on your machine. Learn about other options and next steps in the
             project <Link href="https://github.com/openfga/openfga">README.md</Link> or{' '}
@@ -23,8 +30,11 @@ const QuickStartSection = () => {
             <Link to="./docs/modeling">documentation</Link>.
           </p>
         </div>
-        <div className={styles.graphic}>
-          <QuickStartImg />
+        <div className={styles.terminalContainer}>
+          <video width="646" preload="auto" autoPlay muted loop className={styles.terminalImg} poster={terminalImg}>
+            <source src={terminalWebm} type="video/webm" />
+            <source src={terminalMp4} type="video/mp4" />
+          </video>
         </div>
       </div>
     </Section>
