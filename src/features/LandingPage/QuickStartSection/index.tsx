8427f39b73bf3765b1b74e5cdf34afb7d5896ac5
Matthew Pereira <mail@matthewpereira.com>
2022-06-23 17:29:31 -0300

    feat: copy button for docker snippet

--- src/features/LandingPage/QuickStartSection/index.tsx
+++ src/features/LandingPage/QuickStartSection/index.tsx
@@ -9,6 +9,32 @@ import terminalImg from './terminal.png';
 import styles from './QuickStartSection.module.css';
 
 const QuickStartSection = () => {
+  const processHTMLSnippet = (snippet: string) => {
+    // Change &amp to &
+    const translateAmpersands = snippet.replace(/&amp;/g, '&');
+    // Remove <br /> and \
+    const translateLineBreaks = translateAmpersands.replace(/<br>|\\/g, '');
+
+    return translateLineBreaks;
+  };
+
+  const changeSVGState = () => {
+    document.getElementsByClassName(styles.copyQuickstart)[0].setAttribute('copied', 'true');
+
+    setTimeout(() => {
+      document.getElementsByClassName(styles.copyQuickstart)[0].setAttribute('copied', 'false');
+    }, 5000);
+  };
+
+  const copyCurlCommand = () => {
+    const htmlCommand = document.getElementById('dockerSnippet').innerHTML;
+    const plainCommand = processHTMLSnippet(htmlCommand);
+
+    navigator.clipboard.writeText(plainCommand);
+
+    changeSVGState();
+  };
+
   return (
     <Section className={styles.section} id="quick-start">
       <div className={styles.container}>
@@ -16,10 +42,29 @@ const QuickStartSection = () => {
           <h2 className="section_heading">Quick Start</h2>
           <p className={styles.highlight}>Trying OpenFGA is as easy as...</p>
           <p>Run the following snippet in a terminal in an environment with Docker installed:</p>
-          <pre className={styles.dockerSnippet}>
-            docker pull openfga/openfga && \<br />
-            docker run -p 8080:8080 openfga/openfga run
-          </pre>
+          <div className={styles.copyCodeBlock}>
+            <pre id="dockerSnippet" className={styles.dockerSnippet}>
+              docker pull openfga/openfga && \<br />
+              docker run -p 8080:8080 openfga/openfga run
+            </pre>
+            <button className={styles.copyQuickstart} onClick={() => copyCurlCommand()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#fff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Copy docker command to clipboard</title>
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+            </button>
+          </div>
           <p>
             OpenFGA will be running at localhost:8080 on your machine. Learn about other options and next steps in the
             project <Link href="https://github.com/openfga/openfga">README.md</Link> or{' '}
