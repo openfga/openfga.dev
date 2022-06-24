d79eaff8fafd2742f028413e5e0676cd84f3ba89
Matthew Pereira <mail@matthewpereira.com>
2022-06-24 09:23:51 -0300

    fix: rem units, add helper text

--- src/features/LandingPage/QuickStartSection/index.tsx
+++ src/features/LandingPage/QuickStartSection/index.tsx
@@ -19,10 +19,10 @@ const QuickStartSection = () => {
   };
 
   const changeSVGState = () => {
-    document.getElementsByClassName(styles.copyQuickstart)[0].setAttribute('copied', 'true');
+    document.getElementsByClassName(styles.copyQuickstart)[0].setAttribute('data-copied', 'true');
 
     setTimeout(() => {
-      document.getElementsByClassName(styles.copyQuickstart)[0].setAttribute('copied', 'false');
+      document.getElementsByClassName(styles.copyQuickstart)[0].setAttribute('data-copied', 'false');
     }, 5000);
   };
 
@@ -47,7 +47,7 @@ const QuickStartSection = () => {
               docker pull openfga/openfga && \<br />
               docker run -p 8080:8080 openfga/openfga run
             </pre>
-            <button className={styles.copyQuickstart} onClick={() => copyCurlCommand()}>
+            <button className={styles.copyQuickstart} data-copied="false" onClick={() => copyCurlCommand()}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -59,7 +59,7 @@ const QuickStartSection = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
               >
-                <title>Copy docker command to clipboard</title>
+                <title>Copy quick start command to clipboard</title>
                 <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
                 <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
               </svg>
