8a7bef355fd86baeb52bb76b1a9d37de55bfbff7
Matthew Pereira <mail@matthewpereira.com>
2022-07-15 11:17:52 -0300

    fix: typescript fix and height style for animated icon div

--- src/components/icons/AnimatedIcon.tsx
+++ src/components/icons/AnimatedIcon.tsx
@@ -1,20 +1,19 @@
-import renderRoutes from '@docusaurus/renderRoutes';
 import * as React from 'react';
 import Lottie from 'react-lottie';
 
-const AnimatedIcon = ({animatedIcon}) => (
-  <div>
-    {console.log(animatedIcon)}
-    <Lottie options={{
-      loop: true,
-      autoplay: true,
-      animationData: animatedIcon,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice',
-      },
-    }} />
+const AnimatedIcon = (animatedIcon: Element) => (
+  <div style={{ height: '100%' }}>
+    <Lottie
+      options={{
+        loop: true,
+        autoplay: true,
+        animationData: animatedIcon,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        },
+      }}
+    />
   </div>
 );
-
 
 export { AnimatedIcon };
