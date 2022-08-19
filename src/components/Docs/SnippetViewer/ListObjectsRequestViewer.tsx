b688a67fa9bfd0c8a6aaacd006011ae9466bb249
Maria Ines Parnisari <maineparnisari@gmail.com>
2022-08-19 14:41:38 -0700

    include expected results in ListObjectsRequestViewer + include Streaming version

--- src/components/Docs/SnippetViewer/ListObjectsRequestViewer.tsx
+++ src/components/Docs/SnippetViewer/ListObjectsRequestViewer.tsx
@@ -9,6 +9,7 @@ interface ListObjectsRequestViewerOpts {
   relation: string;
   objectType: string;
   contextualTuples?: TupleKey[];
+  expectedResults: string[];
   skipSetup?: boolean;
   allowedLanguages?: SupportedLanguage[];
 }
@@ -18,7 +19,7 @@ function listObjectsRequestViewer(
   opts: ListObjectsRequestViewerOpts,
   languageMappings: LanguageMappings,
 ): string {
-  const { authorizationModelId, user, relation, objectType, contextualTuples } = opts;
+  const { authorizationModelId, user, relation, objectType, contextualTuples, expectedResults } = opts;
 
   switch (lang) {
     case SupportedLanguage.PLAYGROUND:
@@ -36,86 +37,130 @@ function listObjectsRequestViewer(
           ? `,
         "contextual_tuples": {
           "tuple_keys": [${contextualTuples
-              .map((tuple) => `
-            {"object": "${tuple.object}", "relation": "${tuple.relation}", "user": "${tuple.user}"}`)
-              .join(',')}
+            .map(
+              (tuple) => `
+            {"object": "${tuple.object}", "relation": "${tuple.relation}", "user": "${tuple.user}"}`,
+            )
+            .join(',')}
           ]
         }
       }'`
           : `
       }'`
-        }
+      }
 
-# Response: {"object_ids": ["otherdoc", "planning"]}`;
+# Response: {"object_ids": [${expectedResults.map((r) => `"${r}"`).join(', ')}]}`;
     /* eslint-enable max-len */
     case SupportedLanguage.JS_SDK:
-      return `const response = await fgaClient.listObjects({${authorizationModelId ? `
-  authorization_model_id: "${authorizationModelId}",` : ``}
+      return `const response = await fgaClient.listObjects({${
+        authorizationModelId
+          ? `
+  authorization_model_id: "${authorizationModelId}",`
+          : ``
+      }
   user: "${user}",
   relation: "${relation}",
-  type: "${objectType}",${contextualTuples?.length ? `
+  type: "${objectType}",${
+        contextualTuples?.length
+          ? `
   contextual_tuples: {
-    tuple_keys: [${contextualTuples.map(tupleKey => `{
+    tuple_keys: [${contextualTuples
+      .map(
+        (tupleKey) => `{
       user: "${tupleKey.user}",
       relation: "${tupleKey.relation}",
       object: "${tupleKey.object}"
-    }`).join(', ')}]
-  },` : '' }
+    }`,
+      )
+      .join(', ')}]
+  },`
+          : ''
+      }
 });
-// response.object_ids = ["otherdoc", "planning"]`;
+// response.object_ids = [${expectedResults.map((r) => `"${r}"`).join(', ')}]`;
     case SupportedLanguage.GO_SDK:
       /* eslint-disable no-tabs */
-      return `body := fgaSdk.ListObjectsRequest{${authorizationModelId ? `
-    AuthorizationModelId: PtrString("${authorizationModelId}"),` : ``}
+      return `body := fgaSdk.ListObjectsRequest{${
+        authorizationModelId
+          ? `
+    AuthorizationModelId: PtrString("${authorizationModelId}"),`
+          : ``
+      }
     User:                 PtrString("${user}"),
     Relation:             PtrString("${relation}"),
-    Type:                 PtrString("${objectType}"),${contextualTuples?.length ? `
+    Type:                 PtrString("${objectType}"),${
+        contextualTuples?.length
+          ? `
     ContextualTuples: &ContextualTupleKeys{
-        TupleKeys: []TupleKey{${contextualTuples.map(tupleKey => `{
+        TupleKeys: []TupleKey{${contextualTuples
+          .map(
+            (tupleKey) => `{
             User:     PtrString("${tupleKey.user}"),
             Relation: PtrString("${tupleKey.relation}"),
             Object:   PtrString("${tupleKey.object}"),
-        }`).join(', ')},
-    },` : '' }
+        }`,
+          )
+          .join(', ')},
+    },`
+          : ''
+      }
 }
 
-data, response, err := apiClient.${languageMappings['go'].apiName}.ListObjects(context.Background()).Body(body).Execute()
+data, response, err := apiClient.${
+        languageMappings['go'].apiName
+      }.ListObjects(context.Background()).Body(body).Execute()
 
-// data = { "object_ids": ["otherdoc", "planning"] }`;
+// data = { "object_ids": [${expectedResults.map((r) => `"${r}"`).join(', ')}] }`;
     case SupportedLanguage.DOTNET_SDK:
-      return `var body = new ListObjectsRequest{${authorizationModelId ? `
-    AuthorizationModelId = "${authorizationModelId}",` : ``}
+      return `var body = new ListObjectsRequest{${
+        authorizationModelId
+          ? `
+    AuthorizationModelId = "${authorizationModelId}",`
+          : ``
+      }
     User = "${user}",
     Relation = "${relation}",
-    Type = "${objectType}",${contextualTuples?.length ? `
+    Type = "${objectType}",${
+        contextualTuples?.length
+          ? `
     ContextualTuples = new ContextualTupleKeys() {
-        TupleKeys = new List<TupleKey> {${contextualTuples.map(tupleKey => `
-            new("${tupleKey.object}", "${tupleKey.relation}", "${tupleKey.user}")`).join(',')}
+        TupleKeys = new List<TupleKey> {${contextualTuples
+          .map(
+            (tupleKey) => `
+            new("${tupleKey.object}", "${tupleKey.relation}", "${tupleKey.user}")`,
+          )
+          .join(',')}
         }
-    }` : '' }
+    }`
+          : ''
+      }
 };
 var response = await openFgaApi.ListObjects(body);
 
-// response.ObjectIds = ["otherdoc", "planning"]`;
+// response.ObjectIds = [${expectedResults.map((r) => `"${r}"`).join(', ')}]`;
     case SupportedLanguage.RPC:
       return `listObjects(
   "${user}", // list the objects that the user \`${user}\`
   "${relation}", // has an \`${relation}\` relation
-  "${objectType}", // and that are of type \`${objectType}\`${authorizationModelId ? `
-  authorization_model_id = "${authorizationModelId}", // for this particular authorization model id` : ``}${
-    contextualTuples
-      ? `
+  "${objectType}", // and that are of type \`${objectType}\`${
+        authorizationModelId
+          ? `
+  authorization_model_id = "${authorizationModelId}", // for this particular authorization model id`
+          : ``
+      }${
+        contextualTuples
+          ? `
   contextual_tuples = [ // Assuming the following is true
     ${contextualTuples
       .map((tuple) => `{user = "${tuple.user}", relation = "${tuple.relation}", object = "${tuple.object}"}`)
       .join(',\n    ')}
   ]`
-      : ''
-  }
+          : ''
+      }
 );
 
-Reply: ["otherdoc", "planning"]`;
-   default:
+Reply: [${expectedResults.map((r) => `"${r}"`).join(', ')}]`;
+    default:
       assertNever(lang);
   }
 }
