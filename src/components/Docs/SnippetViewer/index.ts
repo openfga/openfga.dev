51e77cfadc774d0452f3022807c154ddafe13344
Jonathan Whitaker <jon.whitaker@auth0.com>
2022-08-22 11:32:47 -0700

    docs: add ListObjects documentation (#183)
    Co-authored-by: Raghd Hamzeh <raghd.hamzeh@auth0.com>

--- src/components/Docs/SnippetViewer/index.ts
+++ src/components/Docs/SnippetViewer/index.ts
@@ -1,6 +1,7 @@
 export * from './CheckRequestViewer';
 export * from './DefaultTabbedViewer';
 export * from './ExpandRequestViewer';
+export * from './ListObjectsRequestViewer';
 export * from './ReadChangesRequestViewer';
 export * from './ReadRequestViewer';
 export { SdkSetupHeader } from './SdkSetup';
