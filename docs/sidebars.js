fef390033e7dd7b4dc432524ef12208cc275dc86
Andrés Aguiar <andres.aguiar@auth0.com>
2022-12-07 13:27:46 -0800

    feat: adding schema 1.1 migration doc (#290)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -134,11 +134,6 @@ const sidebars = {
           id: 'content/modeling/organization-context-authorization',
         },
         {
-          type: 'doc',
-          label: 'Migrating Relations',
-          id: 'content/modeling/migrating-relations',
-        },
-        {
           type: 'category',
           collapsed: true,
           collapsible: true,
@@ -204,6 +199,25 @@ const sidebars = {
               type: 'doc',
               label: 'Entitlements',
               id: 'content/modeling/advanced/entitlements',
+            },
+          ],
+        },
+        {
+          type: 'category',
+          collapsed: true,
+          collapsible: true,
+          label: 'Migrations',
+          
+          items: [
+            {
+              type: 'doc',
+              label: 'Migrating Relations',
+              id: 'content/modeling/migrating-relations',
+            },
+            {
+              type: 'doc',
+              label: 'Migrating to Schema 1.1',
+              id: 'content/modeling/migrating-schema-1-1',
             },
           ],
         },
