10b270805d7300f2c15eba6a343f343b95438ade
ewanharris <ewanharris@users.noreply.github.com>
2024-02-12 10:41:20 +0000

    Deploy preview for PR 639 🛫

---
+++ pr-preview/pr-639/assets/js/b3e13dba.44248357.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[6840],{3250:e=>{e.exports=JSON.parse('{"label":"openfga","permalink":"/blog/tags/openfga","allTagsPath":"/blog/tags","count":1,"unlisted":false}')}}]);
