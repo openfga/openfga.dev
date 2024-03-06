01e67a71ba1839e434b929beae9685babab3d4e2
rhamzeh <rhamzeh@users.noreply.github.com>
2024-03-06 23:43:57 +0000

    Deploy preview for PR 685 🛫

---
+++ pr-preview/pr-685/assets/js/2168881b.df8a5765.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[1397],{23484:e=>{e.exports=JSON.parse('{"label":"openfga","permalink":"/pr-preview/pr-685/blog/tags/openfga","allTagsPath":"/pr-preview/pr-685/blog/tags","count":1,"unlisted":false}')}}]);
