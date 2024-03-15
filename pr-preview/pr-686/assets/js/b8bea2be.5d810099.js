2a84258eb9ca41bccf171c03363fce9eca60cda9
rhamzeh <rhamzeh@users.noreply.github.com>
2024-03-15 14:10:04 +0000

    Deploy preview for PR 686 🛫

---
+++ pr-preview/pr-686/assets/js/b8bea2be.5d810099.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[2416],{26413:e=>{e.exports=JSON.parse('{"label":"kubecon","permalink":"/pr-preview/pr-686/blog/tags/kubecon","allTagsPath":"/pr-preview/pr-686/blog/tags","count":1,"unlisted":false}')}}]);
