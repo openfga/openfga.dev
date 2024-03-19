485613708dc7ee1faf994fe6c935c8a1ce114827
ewanharris <ewanharris@users.noreply.github.com>
2024-03-19 16:39:45 +0000

    Deploy preview for PR 697 🛫

---
+++ pr-preview/pr-697/assets/js/597f5eb1.4b60f498.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[3474],{5316:e=>{e.exports=JSON.parse('{"label":"kubecon","permalink":"/pr-preview/pr-697/blog/tags/kubecon","allTagsPath":"/pr-preview/pr-697/blog/tags","count":1,"unlisted":false}')}}]);
