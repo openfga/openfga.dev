b68c5f7ffbe1118f54c217c4202a05b942710ec6
rhamzeh <rhamzeh@users.noreply.github.com>
2024-04-22 20:44:40 +0000

    Deploy preview for PR 710 🛫

---
+++ pr-preview/pr-710/assets/js/1e12a81c.f7cf717b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[3639],{47955:e=>{e.exports=JSON.parse('{"label":"openfga","permalink":"/pr-preview/pr-710/blog/tags/openfga","allTagsPath":"/pr-preview/pr-710/blog/tags","count":2,"unlisted":false}')}}]);
---
+++ pr-preview/pr-710/assets/js/51912b17.f34788e3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[2651],{17836:e=>{e.exports=JSON.parse('{"permalink":"/pr-preview/pr-710/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
