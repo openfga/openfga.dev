71b0930f6ffd7b0080b2bf9129579f424395f5db
ryanpq <ryanpq@users.noreply.github.com>
2024-08-30 15:17:44 +0000

    Deploy preview for PR 702 🛫

---
+++ pr-preview/pr-702/assets/js/1d0c735d.2ffaf1d4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[6518],{13444:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-702/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":13,"previousPage":"/pr-preview/pr-702/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
