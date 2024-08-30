6dd3d66d6b90237b4b3779995b7564e1d3f32b60
rhamzeh <rhamzeh@users.noreply.github.com>
2024-08-30 16:03:55 +0000

    Deploy preview for PR 821 🛫

---
+++ pr-preview/pr-821/assets/js/9019ce1c.56f14a59.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[7986],{9068:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-821/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":13,"previousPage":"/pr-preview/pr-821/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
