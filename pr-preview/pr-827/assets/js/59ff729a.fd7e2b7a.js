5dbfa382e2c531f8ffe28df8c57c33609f6e86cd
rhamzeh <rhamzeh@users.noreply.github.com>
2024-09-04 02:03:11 +0000

    Deploy preview for PR 827 🛫

---
+++ pr-preview/pr-827/assets/js/59ff729a.fd7e2b7a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[3832],{5588:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-827/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":14,"previousPage":"/pr-preview/pr-827/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
