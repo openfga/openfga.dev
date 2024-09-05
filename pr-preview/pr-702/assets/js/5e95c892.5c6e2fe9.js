624a1d6f79bfa1a6bfbd623f65c8d7f2568aaad8
ryanpq <ryanpq@users.noreply.github.com>
2024-09-05 17:06:29 +0000

    Deploy preview for PR 702 🛫

---
+++ pr-preview/pr-702/assets/js/5e95c892.5c6e2fe9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(96540);var a=r(34164),n=r(61213),u=r(17559),c=r(22831),d=r(12161),p=r(74848);function t(e){return(0,p.jsx)(n.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,p.jsx)(d.A,{children:(0,c.v)(e.route.routes)})})}}}]);
