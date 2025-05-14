7a617efaba7ed1769a96d24da077d93e2a69d9f2
Talent Zeng <talent.z@hotmail.com>
2025-05-14 11:28:45 -0400

    verification test

--- src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
+++ src/components/Docs/AuthorizationModel/AuthzModelSnippetViewer.tsx
@@ -71,6 +71,7 @@ const AuthzModelSnippetViewer: React.FC<AuthzModelSnippetViewerProps> = ({
       {showWrite ? (
         <details>
           <summary>Write the Authorization Model</summary>
+
           <Admonition type="note">
             <div>The OpenFGA API only accepts an authorization model in the API&#39;s JSON syntax.</div>
             <div>
@@ -78,6 +79,7 @@ const AuthzModelSnippetViewer: React.FC<AuthzModelSnippetViewerProps> = ({
               <Link href={'https://github.com/openfga/cli/'}>FGA CLI</Link>.
             </div>
           </Admonition>
+
           <WriteAuthzModelViewer authorizationModel={{ ...configuration, id: '' }} skipSetup={true} />
         </details>
       ) : undefined}
