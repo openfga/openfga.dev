2c4247b94ee177df567eb72a9aebedbac2e88671
aaguiarz <aaguiarz@users.noreply.github.com>
2026-01-24 20:54:51 +0000

    Deploy preview for PR 1172 🛫

---
+++ pr-preview/pr-1172/assets/js/1a1b1e15.852ddb40.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenfga_dev=self.webpackChunkopenfga_dev||[]).push([[2108],{9729:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-1172/blog","page":1,"postsPerPage":10,"totalPages":3,"totalCount":23,"nextPage":"/pr-preview/pr-1172/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
