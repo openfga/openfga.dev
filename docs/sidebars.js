874a3991f3c61e0756d366749edf7e396527a681
Andres Aguiar <andres.aguiar@okta.com>
2026-04-06 23:08:46 -0300

    feat: added authz for agents as principals and reorderd sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -313,33 +313,6 @@ const sidebars = {
           type: 'category',
           collapsed: true,
           collapsible: true,
-          label: 'Authorization for Agents',
-          link: {
-            type: 'doc',
-            id: 'content/modeling/agents/overview',
-          },
-          items: [
-            {
-              type: 'doc',
-              label: 'Task-Based Authorization',
-              id: 'content/modeling/agents/task-based-authorization',
-            },
-            {
-              type: 'doc',
-              label: 'RAG Authorization',
-              id: 'content/modeling/agents/rag-authorization',
-            },
-            {
-              type: 'doc',
-              label: 'Authorization for MCP Servers',
-              id: 'content/modeling/agents/mcp-authorization',
-            },
-          ],
-        },
-        {
-          type: 'category',
-          collapsed: true,
-          collapsible: true,
           label: 'Migrations',
           link: {
             type: 'doc',
@@ -357,6 +330,38 @@ const sidebars = {
               id: 'content/modeling/migrating/migrating-models',
             },
           ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      collapsible: true,
+      collapsed: false,
+      label: 'Authorization for Agents',
+      link: {
+        type: 'doc',
+        id: 'content/modeling/agents/overview',
+      },
+      items: [
+        {
+          type: 'doc',
+          label: 'Agent Principals in Existing Models',
+          id: 'content/modeling/agents/agent-principals-in-existing-app',
+        },        
+        {
+          type: 'doc',
+          label: 'RAG Authorization',
+          id: 'content/modeling/agents/rag-authorization',
+        },
+        {
+          type: 'doc',
+          label: 'Authorization for MCP Servers',
+          id: 'content/modeling/agents/mcp-authorization',
+        },
+        {
+          type: 'doc',
+          label: 'Task-Based Authorization',
+          id: 'content/modeling/agents/task-based-authorization',
         },
       ],
     },
